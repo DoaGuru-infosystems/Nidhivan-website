3acb45aa17bc1bd2daf0594fa04d6f669fe4c589
Ashish Kumar <ashish9039062705@gmail.com>
2026-07-29 14:59:55 +0530

    refactor: fully modernize Banner.jsx to pure Tailwind, removing legacy parallax classes causing gray strip bug on reload

--- CLIENT/src/components/Elements/Banner.jsx
+++ CLIENT/src/components/Elements/Banner.jsx
@@ -4,34 +4,40 @@ import { NavLink } from 'react-router-dom';
 class Banner extends React.Component {
     render() {
         return (
-            <>
-                <div className="sx-bnr-inr overlay-wraper bg-parallax bg-top-center flex items-center relative h-[500px] bg-cover bg-center bg-scroll" data-stellar-background-ratio="0.5" style={{ backgroundImage: 'url(' + this.props.bgimage + ')', backgroundAttachment: 'scroll' }}>
-                    <div className="overlay-main bg-black/70 absolute inset-0 z-0" />
-                    <div className="max-w-7xl mx-auto px-4 relative z-10 w-full">
-                        <div className="sx-bnr-inr-entry w-full">
-                            <div className="banner-title-outer mb-5">
-                                <div className="banner-title-name">
-                                    <h2 className="m-tb0 text-white text-5xl font-extrabold mb-5">{this.props.title}</h2>
-                                    <p className="text-white max-w-[555px]">
-                                        {this.props.description}
-                                    </p>
-                                </div>
-                            </div>
-                            {/* BREADCRUMB ROW */}
-                            <div>
-                                <ul className="sx-breadcrumb breadcrumb-style-2 flex gap-2 text-white/80">
-                                    <li><NavLink to={"./"} className="hover:text-white transition-colors">Home</NavLink></li>
-                                    <li>/</li>
-                                    <li className="text-white">{this.props.pagename}</li>
-                                </ul>
-                            </div>
-                            {/* BREADCRUMB ROW END */}
-                        </div>
+            <div 
+                className="relative flex items-center justify-center h-[500px] w-full bg-cover bg-center bg-no-repeat" 
+                style={{ backgroundImage: `url(${this.props.bgimage})` }}
+            >
+                {/* Dark Overlay */}
+                <div className="absolute inset-0 bg-black/70 z-0" />
+                
+                {/* Content Container */}
+                <div className="relative z-10 w-full max-w-7xl mx-auto px-4 flex flex-col justify-center">
+                    <div className="mb-5">
+                        <h2 className="text-white text-5xl font-extrabold mb-5 m-0">
+                            {this.props.title}
+                        </h2>
+                        {this.props.description && (
+                            <p className="text-white max-w-[555px]">
+                                {this.props.description}
+                            </p>
+                        )}
+                    </div>
+                    
+                    {/* Breadcrumbs */}
+                    <div>
+                        <ul className="flex flex-wrap items-center gap-2 text-white/80 font-medium text-sm">
+                            <li>
+                                <NavLink to={"/"} className="hover:text-white transition-colors">Home</NavLink>
+                            </li>
+                            <li>/</li>
+                            <li className="text-white">{this.props.pagename}</li>
+                        </ul>
                     </div>
                 </div>
-            </>
+            </div>
         );
     }
-};
+}
 
 export default Banner;
